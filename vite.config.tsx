a7af36a5220ea428860b912d4f10b43945eecff8
Wthomas48 <wthomas19542@gmail.com>
2025-07-06 22:27:20 -0400

    Update vite.config.tsx

--- vite.config.tsx
+++ vite.config.tsx
@@ -1,23 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: './frontend',
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  root: "./", // Current directory (frontend/)
   build: {
-    outDir: "dist",
+    outDir: '../dist',
     emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
-    },
   },
 });
